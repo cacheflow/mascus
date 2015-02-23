bdf7edc177e9855205b9b786400602a6a251bda4
Lex Alexander <l.alexander10@gmail.com>
2015-02-22 23:37:07 -0800

    using capybara and mechanize to scrape things.

---
+++ scrape.rb
@@ -0,0 +1,27 @@
+require "mechanize"
+require "nokogiri"
+require "capybara"
+require "capybara/dsl"
+require "capybara-webkit"
+
+include Capybara::DSL
+Capybara.current_driver = :webkit
+Capybara.run_server = false 
+##right here we're including the Capybara DSL 
+##so we can call many of the methods like visit, find, etc..
+agent = Mechanize.new 
+mech = agent.get("http://www.mascus.com/john+deere/+/+/1,100,relevance,search.html?us=categorypath%3Dagriculture")
+links = []
+mech.links.each do |link| 
+  if link.text.start_with?("John Deere") && link.href.start_with?("/agriculture/") && !link.href.to_s.end_with?("modelgroup.html")
+    page.visit("http://www.mascus.com#{link.href}")
+      if page.has_css?("#phone-number-button")
+        page.find("#phone-number-button").click
+        doc = Nokogiri::HTML.parse(body)
+        puts "NIGGA DA NUMBER IS " + doc.css("#phone-number-button").text 
+        mechanize_parse = agent.get("#{link.href}")
+
+      end           
+  end 
+end 
+
